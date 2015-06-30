0c77b1fd8d898733279bfcf2966ff2c34cd1e974
Gagnon06 <leonard.gagnon@gmail.com>
2015-06-30 13:59:35 -0400

    Kodi now an object

--- index.js
+++ index.js
@@ -23,38 +23,39 @@ var request = require('request');
 // Note two things:
 //   configure port in Settings -> Services -> Webserver -> Port, default is 8080
 //   enable zeroconf in Kodi to use rasbmc.local, otherwise use static IP or write discovery functionality
-var xbmc_host = 'raspbmc.local';
-var xbmc_port = '8080';    // Configured Kodi/XBMC in Settings -> Services -> Webserver -> Port
-var xbmc_json_rpc_url = "http://" + xbmc_host + ":" + xbmc_port + "/jsonrpc";
-var headers = {
-    'User-Agent': 'kodi-controller.js'
-};
 
-// set the HTTP options, we use the POST method, but GET with URL-encoded string is also possible
-var options = {
-    url: xbmc_json_rpc_url,
-    method: 'POST',
-    headers: headers,
-    json: true,
-    gzip: true,
-    body: "{}"
-};
+var Kodi = function (options) {
+    this.host = options.host;
+    this.port = options.port;
 
-// enum of basic remote control button actions. Use Input.ExecuteAction for the full set as below
-var remote_inputs = {};
-    remote_inputs["Back"] = "Input.Back";
-    remote_inputs["Down"] = "Input.Down";
-    remote_inputs["Up"] = "Input.Up";
-    remote_inputs["Left"] = "Input.Left";
-    remote_inputs["Right"] = "Input.Right";
-    remote_inputs["Select"] = "Input.Select";
-    remote_inputs["Home"] = "Input.Home";
-    remote_inputs["Info"] = "Input.Info";
-    remote_inputs["ContextMenu"] = "Input.ContextMenu";
-    remote_inputs["ShowCodec"] = "Input.ShowCodec";
-    remote_inputs["ShowOSD"] = "Input.ShowOSD";
-    remote_inputs["SendText"] = "Input.SendText";
-    remote_inputs["ExecuteAction"] = "Input.ExecuteAction";
+    // set the HTTP options, we use the POST method, but GET with URL-encoded string is also possible
+    this.options = {
+        url: "http://" + this.host + ":" + this.port + "/jsonrpc",
+        method: 'POST',
+        headers: {
+            'User-Agent': 'kodi-controller.js'
+        },
+        json: true,
+        gzip: true,
+        body: "{}"
+    };
+
+    // enum of basic remote control button actions. Use Input.ExecuteAction for the full set as below
+    this.remote_inputs = {};
+    this.remote_inputs["Back"] = "Input.Back";
+    this.remote_inputs["Down"] = "Input.Down";
+    this.remote_inputs["Up"] = "Input.Up";
+    this.remote_inputs["Left"] = "Input.Left";
+    this.remote_inputs["Right"] = "Input.Right";
+    this.remote_inputs["Select"] = "Input.Select";
+    this.remote_inputs["Home"] = "Input.Home";
+    this.remote_inputs["Info"] = "Input.Info";
+    this.remote_inputs["ContextMenu"] = "Input.ContextMenu";
+    this.remote_inputs["ShowCodec"] = "Input.ShowCodec";
+    this.remote_inputs["ShowOSD"] = "Input.ShowOSD";
+    this.remote_inputs["SendText"] = "Input.SendText";
+    this.remote_inputs["ExecuteAction"] = "Input.ExecuteAction";
+};
 // ----------------------------------------
 // Input.Action
 //   "enums": [
@@ -233,11 +234,11 @@ var remote_inputs = {};
 // ----------------------------------------
 // perform a request over HTTP. Basic common functionality that all other commands
 // use to communicate with Kodi.
-var do_request = function(reqpayload, fn) {
-  options.body = reqpayload;
-  console.log("Performing request:" + JSON.stringify(options.body));
+Kodi.prototype.do_request = function(reqpayload, fn) {
+  this.options.body = reqpayload;
+  console.log("Performing request:" + JSON.stringify(this.options.body));
   if (typeof fn === 'function') {
-    return request(options, function (error, response, body) {
+    return request(this.options, function (error, response, body) {
               if (!error && response.statusCode == 200) {
                   // console.log("got ok+callback:" + JSON.stringify(body));
               } else {
@@ -247,7 +248,7 @@ var do_request = function(reqpayload, fn) {
             });
 
   } else {
-    return request(options, function (error, response, body) {
+    return request(this.options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 // console.log("got ok, no callback:" + JSON.stringify(body));
             }
@@ -257,12 +258,12 @@ var do_request = function(reqpayload, fn) {
 // ----------------------------------------
 // Send remote control input to Kodi as defined in the 'remote_inputs' above, eg
 //    send_input(remote_inputs["Back"]);
-var send_input = function(input, fn) {
+Kodi.prototype.send_input = function(input, fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": input,
                  "id": 1};
   // console.log("Sending input");
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     if (!error) {
       console.log("Send input callback got ok, response:" + JSON.stringify(response));
     }
@@ -272,12 +273,12 @@ var send_input = function(input, fn) {
 // ----------------------------------------
 // like send_input, but the full set, see Input.Action list above
 // very useful are eg 'close'.
-var send_input_action = function(inputaction, fn) {
+Kodi.prototype.send_input_action = function(inputaction, fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "Input.ExecuteAction",
                  "params": { "action": inputaction},
                  "id": 1};
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
       if (!error) {
         console.log("Send input action callback got ok, response:" + JSON.stringify(response));
       }
@@ -287,9 +288,9 @@ var send_input_action = function(inputaction, fn) {
 // ----------------------------------------
 // Get a Kodi player instance, needed for querying for playing movies, stop/pause
 // etc, but not for eg set/clear playlist or start movie
-var get_player = function(fn) {
+Kodi.prototype.get_player = function(fn) {
   var payload = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1};
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     if (error) {
         console.log("error getting player:" + JSON.stringify(response));
     } else {
@@ -300,12 +301,12 @@ var get_player = function(fn) {
 };
 // ----------------------------------------
 // Get the movie library
-var get_movie_library = function(fn) {
+Kodi.prototype.get_movie_library = function(fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "VideoLibrary.GetMovies",
                  "id": 1};
   // console.log("Getting movie library information");
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     var ret;
     if (!error) {
       ret = response.result.movies;
@@ -318,12 +319,12 @@ var get_movie_library = function(fn) {
 };
 // ----------------------------------------
 // Get details for a movie using the movieid as retrieved from the movie library, eg 493
-var get_movie_details = function(movieid, fn) {
+Kodi.prototype.get_movie_details = function(movieid, fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "VideoLibrary.GetMovieDetails",
                  "params": { "movieid": parseInt(movieid)},
                  "id": 1};
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     var ret = "";
     if (!error) {
       ret = response.result.moviedetails;
@@ -338,11 +339,11 @@ var get_movie_details = function(movieid, fn) {
 // ----------------------------------------
 // get all playlists. They should/could be 3: movie, audio, pictures.
 // We are only interested in 'movies' and that seems to always be labelled '1'.
-var get_playlists = function(fn) {
+Kodi.prototype.get_playlists = function(fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "Playlist.GetPlaylists",
                  "id": 1};
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     var playlists = response.result;
     if (playlists.length === 0) {
       fn(true, {}); // request went fine but no playlists found
@@ -353,12 +354,12 @@ var get_playlists = function(fn) {
 };
 // ----------------------------------------
 // Get items on a playlist as retrieved using get_playlists(), eg 1
-var get_playlist_items = function(playlistid, fn) {
+Kodi.prototype.get_playlist_items = function(playlistid, fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "Playlist.GetItems",
                  "params": { "playlistid": playlistid},
                  "id": 1};
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     if (!error) {
       var playlistitems = response.result.items;
       fn(error, response);
@@ -369,13 +370,13 @@ var get_playlist_items = function(playlistid, fn) {
 };
 // ----------------------------------------
 // Clear playlist as retrieved using get_playlists(), eg 1
-var clear_playlist = function(playlistid, fn) {
+Kodi.prototype.clear_playlist = function(playlistid, fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "Playlist.Clear",
                  "params": { "playlistid": playlistid},
                  "id": 1};
   // console.log("Clearing playlist");
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     if (!error) {
       console.log("clear playlist callback got ok, response:" + JSON.stringify(response));
     }
@@ -385,7 +386,7 @@ var clear_playlist = function(playlistid, fn) {
 // ----------------------------------------
 // Insert a movie (from library id, eg 493) into a playlist (from get_playlists())
 // at a certain position (zero-indexed -> 0 is the first position)
-var insert_at_playlist = function(playlistid, position, movieid, fn) {
+Kodi.prototype.insert_at_playlist = function(playlistid, position, movieid, fn) {
   var payload = {"jsonrpc": "2.0",
                  "method": "Playlist.Insert",
                  "params": { "playlistid":  playlistid,
@@ -394,7 +395,7 @@ var insert_at_playlist = function(playlistid, position, movieid, fn) {
                              },
                  "id": 1};
   // console.log("Inserting to playlist");
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     if (!error) {
       console.log("Insert at playlist callback got ok, response:" + JSON.stringify(response));
     }
@@ -403,14 +404,14 @@ var insert_at_playlist = function(playlistid, position, movieid, fn) {
 };
 // ----------------------------------------
 // Start a playlist (id as found per above) at playlist position (0 is first)
-var start_playlist = function(playlist, position, fn) {
+Kodi.prototype.start_playlist = function(playlist, position, fn) {
    // open playlist[id][position]
   var payload = {"jsonrpc": "2.0",
                  "method": "Player.Open",
                  "params": { "item": { "playlistid" :  playlist,
                                        "position" : position}},
                  "id": 1};
-  do_request(payload, function(error, response) {
+  this.do_request(payload, function(error, response) {
     if (!error) {
       console.log("start movie callback got ok, response:" + JSON.stringify(response));
     }
@@ -421,8 +422,8 @@ var start_playlist = function(playlist, position, fn) {
 // Start a movie using movie id as found through the library call
 // Clears a playlist and inserts this movie at the first position before starting
 // the playlist.
-var start_movie = function(movieid, fn) {
-  get_playlists(function(err, playlists){
+Kodi.prototype.start_movie = function(movieid, fn) {
+  this.get_playlists(function(err, playlists){
     if (!err) {
       // [{"playlistid":0,"type":"audio"},{"playlistid":1,"type":"video"},{"playlistid":2,"type":"picture"}]
       var playlistid = -1;
@@ -437,17 +438,17 @@ var start_movie = function(movieid, fn) {
         return undefined;
       }
 
-      clear_playlist(playlistid, function(err, response){
+      this.clear_playlist(playlistid, function(err, response){
 
         if (!err && response.result == "OK") {
           // {"id":1,"jsonrpc":"2.0","result":"OK"}
           var position = 0; // first movie in playlist
-          insert_at_playlist(playlistid, position, movieid, function(err, response){
+          this.insert_at_playlist(playlistid, position, movieid, function(err, response){
 
             if (!err && response.result == "OK") {
               // {"id":1,"jsonrpc":"2.0","result":"OK"}
               // start playlist!
-              start_playlist(playlistid, position, function(err, response){
+              this.start_playlist(playlistid, position, function(err, response){
 
                 if (!err && response.result == "OK") {
                   // {"id":1,"jsonrpc":"2.0","result":"OK"}
@@ -477,151 +478,6 @@ var start_movie = function(movieid, fn) {
     }
   }); // get_playlists
 };
-// -----------------------------------------------------------------------------
-// export functions for module use
-exports.send_input = send_input;
-exports.send_input_action = send_input_action;
-exports.get_player = get_player;
-exports.get_movie_library = get_movie_library;
-exports.get_movie_details = get_movie_details;
-exports.get_playlists = get_playlists;
-exports.get_playlist_items = get_playlist_items;
-exports.clear_playlist = clear_playlist;
-exports.insert_at_playlist = insert_at_playlist;
-exports.start_playlist = start_playlist;
-exports.start_movie = start_movie;
-
-// enums
-exports.remote_inputs = remote_inputs;
-// -----------------------------------------------------------------------------
-var debugfunction = function(){
-  console.log("Running the debug function");
-// get_player();
-// get_movie_library();
-// XXXXX TODO play/pause depending on playing or not
-
-  // var movieid_totoro = 493;
-  // var totoro_info = get_movie_details(movieid_totoro);
-  // console.log(JSON.stringify(totoro_info));
-  // var playlists = get_playlists();
-  // console.log(playlists);
-  // var plitems = get_playlist_items(1);
-  // console.log(JSON.stringify(plitems));
-  // insert_at_playlist(1, 0, 493);
 
 
-  // start_movie(493, function(err, response) {
-  //   if (!err) {
-  //       console.log("movie started");
-  //   } else {
-  //       console.log("movie not started, error: " + response);
-  //   }
-  // });
-};
-
-var debuginputfunction = function()
-{
-  // send_input(remote_inputs["Back"]);
-  // send_input(remote_inputs["Down"]);
-  // send_input(remote_inputs["Up"]);
-  // send_input(remote_inputs["Left"]);
-  // send_input(remote_inputs["Right"]);
-  // send_input(remote_inputs["Select"]);
-  // send_input(remote_inputs["Home"]);
-  // send_input(remote_inputs["Info"]); // shows time and poster
-  // send_input(remote_inputs["ContextMenu"]);
-  // send_input(remote_inputs["ShowCodec"]);
-  // send_input(remote_inputs["ShowOSD"]);
-  // send_input(remote_inputs["SendText"]);
-  // send_input(remote_inputs["ExecuteAction"]);
-};
-
-// ----------------
-var stop = function(fn) {
-  send_input_action("stop", function(err, resp) {
-    if (!err) {
-      console.log("stop was ok");
-    } else {
-      console.log("stop was NOT ok");
-    }
-    if (typeof fn === 'function') {
-      fn(err, resp);
-    }
-  });
-};
-// ----------------
-var really_pause = function(fn) {
-  send_input_action("pause", function(err, resp) {
-    if (!err) {
-      console.log("pause was ok");
-    } else {
-      console.log("pause NOT ok");
-    }
-    console.log(resp);
-
-    if (typeof fn === 'function') {
-      fn(err, resp);
-    }
-  });
-
-
-/*
-  get_player(function(err, resp){
-    if (!err) {
-      console.log("get player response:");
-      console.log(resp);
-      var playerid = 1; // XXXX fetch from above
-      // get speed of player
-      var speed = 1; // XXXX;
-      // if non-zero, then we are playing
-      if (speed > 0) {
-        // send play/pause
-        send_input_action("playpause");
-        // XXX can't we just send plain pause? same as playpause?
-        send_input_action("pause");
-//     "pause",
-//     "stop",
-//     "playpause",
-      }
-    }
-    if (typeof fn === 'function') {
-      fn(err, resp);
-    }
-  });
-*/
-//     payload_getplayer = {"jsonrpc": "2.0",
-//                          "method": "Player.GetActivePlayers", "id": 1}
-//     r = do_request(payload_getplayer)
-//     data = json.loads(r.text)
-//     print(r.text)
-
-//     if data['result']:
-//         # play/pause and stop are only available if we found a player (ie playing sth)
-//         player_id = data['result'][0]["playerid"]
-
-//         # send play/pause -----------
-//         payload = {"jsonrpc": "2.0",
-//                    "method": "Player.PlayPause",
-//                    "params": { "playerid": player_id },
-//                    "id": 1}
-//         # r = do_request(payload)
-//         # print('play/pause response: %s' % r.text)
-
-//         # send stop -----------------
-//         payload = {"jsonrpc": "2.0",
-//                    "method": "Player.Stop",
-//                    "params": { "playerid": player_id },
-//                    "id": 1}
-//         # r = do_request(payload)
-//         # print('player stop response: %s' % r.text)
-};
-// ----------------
-
-// really_pause(function(err, resp){
-//   console.log("really paused finished.");
-// });
-
-
-
-
-
+module.exports = Kodi;
